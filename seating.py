4e8a982e88cd1a530310ea8a156b8a176ad744bb
Michael Krebs <michael.krebs@mk29.de>
2024-05-14 12:07:22 +0000

    create rectangles for benches

--- seating.py
+++ seating.py
@@ -3,11 +3,11 @@ from uuid import uuid4
 
 
 class Config:
-    seats_per_table: int = 10 # Number of seats per table
-    num_tables_x: int = 9 # Number of tables next to each other
-    num_tables_y: int = 11 # Number of tables in a row
-    gap_tables_x: int = [20, 40] # Gaps between tables in x direction, alternating through the list
-    gap_tables_y: int = [5,5,5,40] # Gaps between tables in y direction, alternating through the list
+    seats_per_table: int = 8 # Number of seats per table
+    num_tables_x: int = 8 # Number of tables next to each other
+    num_tables_y: int = 15 # Number of tables in a row
+    gap_tables_x: int = [20, 45] # Gaps between tables in x direction, alternating through the list
+    gap_tables_y: int = [5,5,5,45] # Gaps between tables in y direction, alternating through the list
     gap_seats_x: int = 60 # Gap between seats in x direction (even and odd seats) in a bench
     gap_seats_y: int = 25 # Gap between seats in y direction in a bench
     seat_radius: int = 10 # Radius of the seat (pretix property)
@@ -54,19 +54,49 @@ def get_table_position(table: int) -> dict:
         "y": y
     }
 
-def create_table(table: int) -> dict:
+def create_table(table: int) -> tuple[dict, dict]:
     position = get_table_position(table)
     uuid = str(uuid4())
-    return {
+    seats = {
         "row_number": str(table),
         "row_number_position": "both",
         "uuid": uuid,
         "position": position,
         "seats": [create_seat(table, i) for i in range(1, config.seats_per_table + 1)]
     }
+    rectangle = {
+        "shape": "rectangle",
+        "color": "#cccccc",
+        "border_color": "#000000",
+        "rotation": 0,
+        "uuid": "5fab97bc-e024-418c-8406-bcfeb8c8a17a",
+        "position": {
+        "x": position["x"],
+        "y": position["y"] - config.seat_radius
+        },
+        "text": {
+        "position": {
+            "x": position["x"],
+            "y": position["y"]
+        },
+        "color": "#333333",
+        "text": ""
+        },
+        "rectangle": {
+        "width": config.gap_seats_x,
+        "height": config.gap_seats_y * (config.seats_per_table / 2 - 1) + config.seat_radius * 2
+        }
+    }
+    return seats, rectangle
 
 def create_seating() -> dict:
-    seating = [create_table(i) for i in range(1, config.num_tables_x * config.num_tables_y + 1)]
+    rows = []
+    areas = []
+    for i in range(1, config.num_tables_x * config.num_tables_y + 1):
+        seats, rectangle = create_table(i)
+        rows.append(seats)
+        areas.append(rectangle)
+    
     return {
         "name": "Seating",
         "categories": [],
@@ -77,15 +107,15 @@ def create_seating() -> dict:
                     "x": 0,
                     "y": 0
                 },
-                "areas": [],
+                "areas": areas,
                 "uuid": str(uuid4()),
                 "zone_id": "Main",
-                "rows": seating
+                "rows": rows
             }
         ],
         "size": {
-            "width": max([table["position"]["x"] + seat["position"]["x"] for table in seating for seat in table["seats"]]) + config.seat_radius + config.margin * 2,
-            "height": max([table["position"]["y"] + seat["position"]["y"] for table in seating for seat in table["seats"]]) + config.seat_radius + config.margin * 2
+            "width": max([table["position"]["x"] + seat["position"]["x"] for table in rows for seat in table["seats"]]) + config.seat_radius + config.margin * 2,
+            "height": max([table["position"]["y"] + seat["position"]["y"] for table in rows for seat in table["seats"]]) + config.seat_radius + config.margin * 2
         }
     }
 
